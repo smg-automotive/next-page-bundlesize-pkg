3682ce65ee068b2760cd58bbfaa5c240d807cc48
Krzysztof Sakwerda <ksakwerda@gmail.com>
2022-08-02 13:01:50 +0200

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: ['@smg-automotive/eslint-config'],
+  parser: '@typescript-eslint/parser',
+};
---
+++ rollup.config.js
@@ -0,0 +1,33 @@
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import dts from 'rollup-plugin-dts';
+import typescript from '@rollup/plugin-typescript';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+
+const packageJson = require('./package.json');
+
+export default [
+  {
+    input: 'src/index.ts',
+    output: [
+      {
+        file: packageJson.main,
+        format: 'cjs',
+        sourcemap: true,
+      },
+      {
+        file: packageJson.module,
+        format: 'esm',
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      peerDepsExternal(),
+      resolve(),
+      commonjs(),
+      typescript({ tsconfig: './tsconfig.build.json' }),
+      dts(),
+    ],
+    external: ['react', 'react-dom'],
+  },
+];
